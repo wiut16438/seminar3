2b7014535941d4cd203a98d85c31916ca5b51a55
Azamat <wiut16438@gmail.com>
2023-09-27 14:17:29 +0500

    task completed

---
+++ seminar3.py
@@ -0,0 +1,48 @@
+# userInput = int(input("Type any number: "))
+
+# def hex_to_decimal(hex_number):
+#     decimal = 0
+#     power = 0
+#
+#     for digit in reversed(hex_number):
+#         if digit.isdigit():
+#             decimal += int(digit) * (16 ** power)
+#         else:
+#             decimal += (ord(digit.upper() - 55)) * (16 ** power)
+#
+#         power += 1
+#
+#     return decimal
+#
+#
+# print(hex_to_decimal('4F'))
+
+# Task5
+
+# userInput = int(input("Type any number: "))
+#
+#
+# def dec_to_hex(input):
+#     hex_number = hex(input)
+#     return hex_number
+#
+# print(dec_to_hex(userInput))
+
+# decValue = input("Type any decimal number: ")
+# binaryValue = bin(int(decValue))
+#
+# newValue = (binaryValue.replace("0b", ""))
+#
+# print(newValue)
+
+# decValue = input("Type any decimal number: ")
+# hexValue = hex(int(decValue))
+# newValue = (hexValue.replace("0x", ""))
+#
+# print(newValue)
+
+# decValue = input("Type any decimal number: ")
+# hexValue = hex(int(decValue))
+# newValue = (hexValue.replace("0x", ""))
+#
+# print(newValue)
